7619c460733bbf8da584e2315b05f0c5a356eea0
cjimoveis <160500116+cjimoveis@users.noreply.github.com>
2024-02-22 20:10:02 -0300

    Delete listings directory

--- listings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='listings'),
-    path('search', views.search, name='search'),
-    path('<int:listing_id>', views.listing, name='listing')
-]
-
